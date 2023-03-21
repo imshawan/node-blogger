77fd12bb78af540c2b7fbde71eee58aad6e05ee1
Shawan Mandal <shawan.sm049@gmail.com>
2023-03-21 17:33:19 +0000

    Configured passport

--- src/server.ts
+++ src/server.ts
@@ -25,99 +25,114 @@ import {initializeDbConnection, database} from './database';
 import { cookies } from './meta';
 import config from '../config.json';
 import _ from 'lodash';
+import passport from 'passport';
+import passportLocal from 'passport-local';
 
 const app = express();
 const httpServer = http.createServer(app);
 const start = async function (port: Number, callback: Function) {
 
-  validateConfiguration(config)
-  await initializeDbConnection(config.mongodb);
-  await setupExpressServer(app);
+    validateConfiguration(config)
+    await initializeDbConnection(config.mongodb);
+    await setupExpressServer(app);
 
-  // Show routes called in console during development
-  if (EnvVars.NodeEnv === NodeEnvs.Dev) {
-      app.use(morgan('dev'));
-  }
+    // Show routes called in console during development
+    if (EnvVars.NodeEnv === NodeEnvs.Dev) {
+        app.use(morgan('dev'));
+    }
 
-  // Security
-  if (EnvVars.NodeEnv === NodeEnvs.Production) {
-      app.use(helmet());
-  }
+    // Security
+    if (EnvVars.NodeEnv === NodeEnvs.Production) {
+        app.use(helmet());
+    }
 
-  // Add APIs, must be after middleware
-  app.use('/', baseRouter);
+    // Add APIs, must be after middleware
+    app.use('/', baseRouter);
 
-  // Add error handler
-  app.use((err: Error, _: Request, res: Response,
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      next: NextFunction,
-  ) => {
-      if (EnvVars.NodeEnv !== NodeEnvs.Test) {
-          logger.err(err, true);
-      }
-      let status = HttpStatusCodes.BAD_REQUEST;
-      if (err instanceof RouteError) {
-          status = err.status;
-      }
+    // Add error handler
+    app.use((err: Error, _: Request, res: Response,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        next: NextFunction,
+    ) => {
+        if (EnvVars.NodeEnv !== NodeEnvs.Test) {
+            logger.err(err, true);
+        }
+        let status = HttpStatusCodes.BAD_REQUEST;
+        if (err instanceof RouteError) {
+            status = err.status;
+        }
 
-      if (res && res.req) {
-          return handleApiResponse(status, res, err);
-      }
+        if (res && res.req) {
+            return handleApiResponse(status, res, err);
+        }
 
-      return res.status(status).json({
-          error: err.message,
-      });
-  });
+        return res.status(status).json({
+            error: err.message,
+        });
+    });
 
-  app.listen(port, () => {
-      if (callback && typeof callback == 'function') {
-          callback()
-      }
-  });
+    app.listen(port, () => {
+        if (callback && typeof callback == 'function') {
+            callback()
+        }
+    });
 }
 
 const destroy = (callback: Function) => {
-  httpServer.close(() => {
-      if (callback && typeof callback == 'function') {
-          callback();
-      }
-  });
+    httpServer.close(() => {
+        if (callback && typeof callback == 'function') {
+            callback();
+        }
+    });
 }
 
 async function setupExpressServer(app: Application) {
-  const expressUserAgent = useragent.express();
-  const secret = _.get(config, 'secret') || '';
+    const expressUserAgent = useragent.express();
+    const secret = _.get(config, 'secret') || '';
 
-  // View engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'ejs');
+    // View engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
 
-  // Set static directory (js and css).
-  const staticDir = path.join('public');
-  app.use(express.static(staticDir));
+    // Set static directory (js and css).
+    const staticDir = path.join('public');
+    app.use(express.static(staticDir));
 
-  // Basic middleware
-  app.use(express.json({
-      limit: '50mb'
-  }));
-  app.use(express.urlencoded({
-      extended: true,
-      limit: '50mb'
-  }));
-  app.use(cookieParser(EnvVars.CookieProps.Secret));
-  app.use(overrideRender);
-  app.use(function userAgent(req, res, next) {
-      expressUserAgent(req, res, next);
-  });
+    // Basic middleware
+    app.use(express.json({
+        limit: '50mb'
+    }));
+    app.use(express.urlencoded({
+        extended: true,
+        limit: '50mb'
+    }));
+    app.use(cookieParser(secret));
+    app.use(overrideRender);
+    app.use(function userAgent(req, res, next) {
+        expressUserAgent(req, res, next);
+    });
 
-  app.use(expressSession({
-      store: database.sessionStore,
-      secret: secret,
-      // key: 'express.sid',
-      cookie: cookies.setupCookie(),
-      resave: false,
-      saveUninitialized: false,
-  }));
+    app.use(expressSession({
+        store: database.sessionStore,
+        secret: secret,
+        // key: 'express.sid',
+        cookie: cookies.setupCookie(),
+        resave: false,
+        saveUninitialized: false,
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // passport.serializeUser(function(user: any, done) {
+    //     done(null, user._id);
+    // });
+    
+    // passport.deserializeUser(async function(id, done) {
+    //     User.findById(id, function (err, user) {
+    //         done(err, user);
+    //         });
+    // });
 }
 
 export default {
