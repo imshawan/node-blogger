ae70973c4fd754d85befbee4053d25169e5e50d5
Shawan Mandal <shawan.sm049@gmail.com>
2023-04-11 08:16:05 +0530

    Added types for category

---
+++ src/types/category.ts
@@ -0,0 +1,20 @@
+export interface ICategory {
+    cid: number
+    parent: number
+    userid: number
+    name: string
+    description: string
+    blurb: string
+    slug: string
+    thumb: string
+    counters: Counters
+    isActive: boolean
+    deleted: boolean
+    createdAt: string
+    updatedAt: string
+}
+
+interface Counters {
+    posts: number
+    tags: number
+}
