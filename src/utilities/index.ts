c80dbac669a6b33c03497070faa011bb2747797c
Shawan Mandal <shawan.sm049@gmail.com>
2023-08-25 00:24:44 +0530

    Added collapseible for session details section. Updates in geolocation resolver response

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -18,22 +18,25 @@ export const capitalizeFirstLetter = (string: string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export const resolveGeoLocation = async (ipAddr: string): Promise<string> => {
+export const resolveGeoLocation = async (ipAddr: string): Promise<{[key: string]: any;}> => {
     const ipLookupUrl = 'http://ip-api.com/json/';
     const response: any = await axios.get(`${ipLookupUrl}${ipAddr}`);
-    let geoLocation = 'Unknown';
+    const geoLocation = {
+        city: 'Unknown',
+        country: 'Unknown',
+        countryCode: '',
+    };
 
     if (response.data && Object.keys(response.data).length) {
-        let ipLookup = [];
+        
         if (Object.hasOwnProperty.bind(response.data)('city')) {
-            ipLookup.push(response.data.city)
+            geoLocation.city = response.data.city;
         }
         if (Object.hasOwnProperty.bind(response.data)('country')) {
-            ipLookup.push(response.data.country);
+            geoLocation.country = response.data.country;
         }
-        
-        if (ipLookup.length) {
-            geoLocation = ipLookup.join(', ');
+        if (Object.hasOwnProperty.bind(response.data)('countryCode')) {
+            geoLocation.country = response.data.country;
         }
     }
 
