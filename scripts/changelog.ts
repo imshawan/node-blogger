842602fdc34853805420018138430f86075e0377
Shawan Mandal <shawan.sm049@gmail.com>
2023-09-29 00:29:35 +0530

    Updated the changelog script

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -9,6 +9,19 @@
  * This changelog file is typically used to document changes, additions, and updates in the project.
  */
 
+import { execSync } from 'child_process';
 import { Changelog } from '../src/utilities/changelog';
+import pkg from '../package.json';
 
-new Changelog().write();
+const gitStatusOutput = execSync('git status --porcelain').toString('utf-8').trim();
+
+if (gitStatusOutput.trim()) {
+    console.log(`The repository has ${gitStatusOutput.split('\n').length} modified file(s). Please commit the existing files and than proceed.`);
+    process.exit(1);
+    
+} else {
+    new Changelog().write();
+
+    execSync('git add CHANGELOG.md');
+    execSync(`git commit -m "(Changelog CI) Latest Changelogs for ${pkg.name} v${pkg.version}"`);
+}
