512b6d110c84ae7c4ccb6d7d7f89a8cd23f8d3eb
Shawan Mandal <shawan.sm049@gmail.com>
2023-10-02 00:25:41 +0530

    #update: Restructured admin side JS to follow the same structure

--- public/scripts/modules/admin.js
+++ public/scripts/modules/admin.js
@@ -1,11 +1,25 @@
-$(document).ready(function () {
-   attachEvents();
-});
+'use strict';
 
-function attachEvents() {
-    // Custom redirection for sidebar
-    $('[data-href]').on('click', function() {
-        const {href} = $(this).data();
-        location.href = href;
-    })
-}
+(function (factory) {
+    window.admin = factory();
+}(function () {
+    const admin = {};
+
+    admin.bootstrap = function bootstrap () {
+        admin.attachEvents();
+    }
+
+    admin.attachEvents = function attachEvents() {
+        // Custom redirection for sidebar
+        $('[data-href]').on('click', function() {
+            const {href} = $(this).data();
+            location.href = href;
+        });
+    }
+
+    $(document).ready(function () {
+        admin.bootstrap();
+     });
+
+    return admin;
+}));
