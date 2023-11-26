915738a717f6be4c260415084c6511dbebb1878c
Shawan Mandal <hello@imshawan.dev>
2023-11-26 11:21:15 +0530

    #update: Updated logger or better output

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -11,37 +11,38 @@ export class Logger {
         this.prefix = options.prefix;
     }
 
-    log = (...message: Array<any>) => {
-        const parsedMessage = this.#parseMessage(message);
-        console.log(this.#timeStamp(), parsedMessage);
-    }
-
-    info = (...message: Array<any>) => {
-        const parsedMessage = this.#parseMessage(message);
-        console.info(this.#timeStamp(), chalk.rgb(255,250,224)(parsedMessage));
-    }
-
-    warn = (...message: Array<any>) => {
-        const parsedMessage = this.#parseMessage(message);
-        console.warn(this.#timeStamp(), chalk.yellow(parsedMessage));
-    }
-
-    error = (...message: Array<any>) => {
-        const parsedMessage = this.#parseMessage(message);
-        console.error(this.#timeStamp(), chalk.redBright(parsedMessage));
-    }
-
-    success = (...message: Array<any>) => {
-        const parsedMessage = this.#parseMessage(message);
-        console.info(this.#timeStamp(), chalk.greenBright(parsedMessage));
-    }
-
-    #timeStamp() {
+    private timeStamp() {
         return `[${new Date(Date.now()).toLocaleString()}]`;
     }
 
-    #parseMessage(message: Array<any>) {
+    /**
+     * @date 26-11-2023
+     * @returns Caller name (equivalent to arguments.callee.name)
+     */
+    private method() {
+        let error = new Error('Error');
+        let funcErrorStack = error.stack && error.stack.split('\n')[2].split(' ');
+        let callerPrevIndex = funcErrorStack && funcErrorStack.indexOf('at');
+        let callerName = (funcErrorStack && callerPrevIndex) && funcErrorStack[callerPrevIndex + 1];
+
+        return callerName ? callerName.split('.')[1] :  undefined;
+    }
+
+    private color(code: string) {
+        switch (code) {
+            case 'error':
+                return 'redBright';
+            case 'warn':
+                return 'yellow';
+            default:
+                return 'greenBright';
+        }
+    }
+
+    private parseMessage(message: Array<any>, prefix: string | undefined) {
         var parsedMessage;
+        prefix = this.prefix || prefix;
+
         if (message && Array.isArray(message) && message.length) {
             parsedMessage = message.map(elem => {
                 if (typeof elem === 'object') {
@@ -50,6 +51,31 @@ export class Logger {
             }).join(' ');
         }
         
-        return this.prefix ? `${chalk.greenBright(`[${this.prefix}]`)} ${parsedMessage}` : parsedMessage;
+        return prefix ? `${chalk[this.color(prefix)](`[${this.prefix || prefix}]`)} ${parsedMessage}` : parsedMessage;
+    }
+
+    log = (...message: Array<any>) => {
+        const parsedMessage = this.parseMessage(message, this.method());
+        console.log(this.timeStamp(), parsedMessage);
+    }
+
+    info = (...message: Array<any>) => {
+        const parsedMessage = this.parseMessage(message, this.method());
+        console.info(this.timeStamp(), chalk.rgb(255,250,224)(parsedMessage));
+    }
+
+    warn = (...message: Array<any>) => {
+        const parsedMessage = this.parseMessage(message, this.method());
+        console.warn(this.timeStamp(), chalk.yellow(parsedMessage));
+    }
+
+    error = (...message: Array<any>) => {
+        const parsedMessage = this.parseMessage(message, this.method());
+        console.error(this.timeStamp(), chalk.redBright(parsedMessage));
+    }
+
+    success = (...message: Array<any>) => {
+        const parsedMessage = this.parseMessage(message, this.method());
+        console.info(this.timeStamp(), chalk.greenBright(parsedMessage));
     }
 }
