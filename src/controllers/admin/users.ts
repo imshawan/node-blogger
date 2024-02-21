dff3e44ef8567b560e1be2da9843bd7d9d350b63
Shawan Mandal <hello@imshawan.dev>
2024-02-21 21:50:04 +0530

    #fix: Fixed submenus issue in users management screen

--- src/controllers/admin/users.ts
+++ src/controllers/admin/users.ts
@@ -3,12 +3,13 @@ import { MutableObject } from "@src/types";
 import {data as sidebarData} from "./sidebar";
 import { SideBar } from "@src/utilities";
 import { getUsersByPagination } from "@src/user";
+import menus from "./sidebar/menus";
 
 const users: MutableObject = {}
 const BASE = 'users'
 
 users.get = async function get(req: Request, res: Response, next: NextFunction) {
-    const sidebar = new SideBar(sidebarData);
+    const sidebar = new SideBar(sidebarData, menus);
     const pageData: MutableObject = {};
 
     pageData.title = 'Users';
