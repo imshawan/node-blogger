665f5070ea8ceb7bea96712368b7b15136d2be75
Shawan Mandal <shawan.sm049@gmail.com>
2024-05-19 14:26:10 +0530

    #update: Added translations for blog module

--- src/locales/namespaces.ts
+++ src/locales/namespaces.ts
@@ -1,17 +1,17 @@
 /**
  * @date 18-05-2024
  * @author imshawan <github@imshawan.dev>
- * 
+ *
  * @description List of namespaces used in the application for i18next translations.
  * Each namespace corresponds to a different section or module of the application.
  * For example: 'common' for common translations, 'consent' for consent related translations, etc.
  */
-const namespaces: string[] = ['common', 'consent', 'user', 'post', 'about'];
+const namespaces: string[] = ['common', 'consent', 'user', 'post', 'blog'];
 
 /**
  * Default namespace used when a namespace is not specified explicitly.
  * Typically, this is the namespace for common translations.
  */
-const defaultNS: string = 'common';
+const defaultNS: string = 'common'
 
-export default {namespaces, defaultNS} as const;
+export default { namespaces, defaultNS } as const;
